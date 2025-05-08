9bb408290ac28c0c192e40f32ac5367bf4ddc7c3
ivankurochenko <hey.ivan.kurochenko@gmail.com>
2025-05-08 13:30:31 +0300

    Cleanup code.

--- src/util/index.js
+++ src/util/index.js
@@ -1,5 +1,4 @@
 export const sendMessage = (message) => {
-    fetch('https://meow-app-backend.onrender.com/what-she-said', {method: 'POST', body: JSON.stringify(message)})
-        .then(res => res.json())
-        .catch(err => console.log(err));
+    fetch('https://meow-app-backend.onrender.com/what-she-said',
+        {method: 'POST', body: JSON.stringify(message)})
 }
