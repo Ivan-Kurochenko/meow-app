9a8fe950537b9c7cea420a2c6c99f899d97a9fbe
ivankurochenko <hey.ivan.kurochenko@gmail.com>
2025-06-22 14:35:07 +0300

    Update generating algorithm.

--- src/feautures/CountFlowers/components/FlowerBouquet.jsx
+++ src/feautures/CountFlowers/components/FlowerBouquet.jsx
@@ -3,43 +3,43 @@ import Flower from "./Flower.jsx";
 const flowersEmoji = ["🌸", "🌼", "🌻", "🌹", "🌺", "🪻"];
 
 function getRandomFlowerEmoji() {
-  return flowersEmoji[Math.floor(Math.random() * flowersEmoji.length)];
+    return flowersEmoji[Math.floor(Math.random() * flowersEmoji.length)];
 }
 
 function getRandomPosition() {
-  const top =
-    Math.random() < 0.5
-      ? Math.floor(Math.random() * 45) + 1
-      : Math.floor(Math.random() * 36) + 55;
-  const left =
-    Math.random() < 0.5
-      ? Math.floor(Math.random() * 34) + 1
-      : Math.floor(Math.random() * 30) + 60;
+    const top =
+        Math.random() < 0.5
+            ? Math.floor(Math.random() * 10) + 30
+            : Math.floor(Math.random() * 60) + 15;
+    const left =
+        Math.random() < 0.5
+            ? Math.floor(Math.random() * 10) + 30
+            : Math.floor(Math.random() * 60) + 15;
 
-  return { top, left };
+    return {top, left};
 }
 
-function FlowerBouquet({ totalFlowers }) {
-  const flowers = Array.from({ length: totalFlowers }, () => {
-    const emoji = getRandomFlowerEmoji();
-    const { top, left } = getRandomPosition();
-    return { emoji, top, left };
-  });
+function FlowerBouquet({totalFlowers}) {
+    const flowers = Array.from({length: totalFlowers}, () => {
+        const emoji = getRandomFlowerEmoji();
+        const {top, left} = getRandomPosition();
+        return {emoji, top, left};
+    });
 
-  return (
-    <div
-      style={{
-        position: "relative",
-        width: "100vw",
-        height: "100vh",
-        overflow: "hidden",
-      }}
-    >
-      {flowers.map((flower, i) => (
-        <Flower key={i} {...flower} />
-      ))}
-    </div>
-  );
+    return (
+        <div
+            style={{
+                position: "relative",
+                width: "100vw",
+                height: "100vh",
+                overflow: "hidden",
+            }}
+        >
+            {flowers.map((flower, i) => (
+                <Flower key={i} {...flower} />
+            ))}
+        </div>
+    );
 }
 
 export default FlowerBouquet;
