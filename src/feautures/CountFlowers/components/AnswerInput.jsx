ca401026f911e5cbf82de0281da76deeff37dd82
ivankurochenko <hey.ivan.kurochenko@gmail.com>
2025-06-22 14:41:15 +0300

    Add CNAME file.

--- src/feautures/CountFlowers/components/AnswerInput.jsx
+++ src/feautures/CountFlowers/components/AnswerInput.jsx
@@ -1,30 +1,35 @@
-import {useState} from "react";
+import { useState } from "react";
 
-const AnswerInput = ({correctFlowers, onSubmit}) => {
-    const [value, setValue] = useState()
+const AnswerInput = ({ correctFlowers, onSubmit }) => {
+  const [value, setValue] = useState();
 
-    const handleSubmit = () => {
-        console.log(correctFlowers);
+  const handleSubmit = () => {
+    console.log(correctFlowers);
 
-        onSubmit(correctFlowers);
-    }
+    onSubmit(correctFlowers);
+  };
 
-    return (
-        <div
-            style={{
-                position: "absolute",
-                top: "50%",
-                left: "50%",
-                transform: "translate(-50%, -50%)",
-                zIndex: 9999,
-                display: "flex",
-                flexDirection: "row",
-            }}
-        >
-            <input type="text" value={value} onChange={setValue} placeholder="Count the flowers"/>
-            <button onClick={handleSubmit}>Submit</button>
-        </div>
-    );
+  return (
+    <div
+      style={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        zIndex: 9999,
+        display: "flex",
+        flexDirection: "row",
+      }}
+    >
+      <input
+        type="text"
+        value={value}
+        onChange={setValue}
+        placeholder="Count the flowers"
+      />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
 };
 
 export default AnswerInput;
