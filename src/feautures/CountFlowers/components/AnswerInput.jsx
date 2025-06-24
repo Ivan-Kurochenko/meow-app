d1bd910f42bbad34e7ad0f3e256bc92e7ab675c5
ivankurochenko <hey.ivan.kurochenko@gmail.com>
2025-06-24 19:26:13 +0300

    Update answer input.

--- src/feautures/CountFlowers/components/AnswerInput.jsx
+++ src/feautures/CountFlowers/components/AnswerInput.jsx
@@ -1,39 +1,43 @@
-import { useState } from "react";
+import {useState} from "react";
 
-const AnswerInput = ({ totalFlowers, onCorrectAnswer }) => {
-  const [answer, setAnswer] = useState(0);
+const AnswerInput = ({totalFlowers, onCorrectAnswer}) => {
+    const [answer, setAnswer] = useState(0);
 
-  const handleSubmit = () => {
-    if (answer === totalFlowers) {
-      onCorrectAnswer(totalFlowers);
-    } else {
-      alert("Incorrect answer!");
-    }
-  };
+    const handleSubmit = () => {
+        if (answer === totalFlowers) {
+            onCorrectAnswer(totalFlowers);
+        } else {
+            alert("Incorrect answer!");
+        }
+    };
 
-  return (
-    <div
-      style={{
-        position: "absolute",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-        zIndex: 9999,
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <label>Count the flowers</label>
-      <div style={{ display: "flex", alignItems: "center" }}>
-        <input
-          type="number"
-          value={answer}
-          onChange={(e) => setAnswer(Number(e.target.value))}
-        />
-        <button onClick={handleSubmit}>Submit</button>
-      </div>
-    </div>
-  );
+    return (
+        <div
+            style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                zIndex: 9999,
+                display: "flex",
+                flexDirection: "column",
+            }}
+        >
+            <label>Count the flowers</label>
+            <div style={{display: "flex", alignItems: "center"}}>
+                <input
+                    type="tel"
+                    value={answer}
+                    onChange={(e) => {
+                        if (!isNaN(Number(e.target.value))) {
+                            setAnswer(Number(e.target.value))
+                        }
+                    }}
+                />
+                <button onClick={handleSubmit}>Submit</button>
+            </div>
+        </div>
+    );
 };
 
 export default AnswerInput;
